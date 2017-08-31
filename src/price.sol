3bc6c52d952dc6703e499c17788fbaf3a6260dfe
Mariano Conti <nanexcool@gmail.com>
2017-08-30 23:45:15 -0300

    Remove dapphub license for now

--- src/price.sol
+++ src/price.sol
@@ -1,14 +1,3 @@
-/// price feed, with expiration and medianizer poke
-
-// Copyright (C) 2017  DappHub, LLC
-
-// Licensed under the Apache License, Version 2.0 (the "License").
-// You may not use this file except in compliance with the License.
-
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND (express or implied).
-
 pragma solidity ^0.4.15;
 
 import "ds-thing/thing.sol";
